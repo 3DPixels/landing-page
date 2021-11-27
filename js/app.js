791374997934d3c345463294a7aae6908c4fe9e0
Ammar Ahmed <ammar.salim2018@gmail.com>
2021-11-27 10:52:11 +0200

    changed active class behavior for viewport

--- js/app.js
+++ js/app.js
@@ -107,10 +107,9 @@ function topFunction() {
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+       
+        
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
     );
 }
 
